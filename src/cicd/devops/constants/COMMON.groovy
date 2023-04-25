328b0ed05ab9b76e525ee29c590b643cbce28559
R.Dasarathi <dasarathi.rout@live.com>
2023-04-25 21:06:38 +0530

    CM-003 : Refactor Stages

--- src/cicd/devops/constants/COMMON.groovy
+++ src/cicd/devops/constants/COMMON.groovy
@@ -5,7 +5,7 @@ class COMMON {
     public static final String MESSAGE_BUILD_FAILED = "############## BUILD FAILED #############"
     public static final String MESSAGE_BUILD_PASSED = "============== BUILD PASSED ============="
     public static final String MESSAGE_STAGE_FINISHED = "@@@@@@@@@@@@@ STAGE FINISHED @@@@@@@@@@@@"
-    public static final String MESSAGE_LOG_LINE = "============================"
+    public static final String MESSAGE_LOG_LINE = " ============== "
 
     public static final String ARTIFACT_GROUP_NAME = "io.rout.dasarathi"
 }
